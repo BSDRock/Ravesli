661708f0a0b716cd953b25f70b9399ee3847c723
Hacker <fvgddg@protonmail.com>
2020-09-02 06:32:16 -0400

    Excess file delited

---
+++ 41r.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+int comma(int x, int y, int y)
+{
+	
+}
+int main()
+{
+	int a=78;
+	int b=87;
+	int c=90;
+    double t = 7.0;
+    std::cout << sizeof(t); // выводим размер переменной t в байтах
+comma(a, b, c);
+return 0;
+
+}
